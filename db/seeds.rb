3206a862d8a2d6f00cd80704853807cabe9770f9
Zachtreis <zacharie.treister@gmail.com>
2019-01-30 22:16:42 +0100

    tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+# User.destroy_all
+
+# #Create new users
+# 5.times do
+#   user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email)
+# end
+
+#create new articles
+# 5.times do
+#   article = Article.create!(user_id: rand(1..10), title: Faker::Lorem.word, content: Faker::Lorem.paragraph)
+# end
+
+#create new categories
+# 4.times do
+#   category = Category.create!(name: Faker::Lorem.word)
+# end
+
+#create new comments
+14.times do
+  commentaire = Commentaire.create!(user_id: rand(1..12), article_id: rand(9..13), content: Faker::ChuckNorris.fact)
+end
+
+#create new likes
+15.times do
+  like = Like.create!(user_id: rand(1..12), article_id: rand(9..13))
+end
